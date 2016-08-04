b10e5f411bad5d4de1eed5ede87cee96ecd0a5d0
orangemug <info@orangemug.co.uk>
2016-08-04 11:19:39 +0100

    Added tests

---
+++ test/index.sh
@@ -0,0 +1,11 @@
+source "$(dirname "$BASH_SOURCE")/../req.sh"
+
+req bash-assert
+
+# Basically just tests if assert module exists...
+assert "a" == "a"
+
+req ./lib/foo
+
+# Test if can require relative
+assert $(foo) == "hello"
